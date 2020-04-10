61dd37eedfe42f46c5b6464249ea851b97805a1a
harinirao1998 <30483498+harinirao1998@users.noreply.github.com>
2020-04-10 09:46:39 +0530

    Delete dataframe.py

--- dataframe.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-df_train = pd.read_csv('train_split_Depression_AVEC2017.csv')
-
-df_test = pd.read_csv('dev_split_Depression_AVEC2017.csv')
-
-df_dev = pd.concat([df_train, df_test], axis=0)
